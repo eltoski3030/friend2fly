70c123b08ba71410f40cb6136a467a447e2a9faa
Pablolizardi <pabloagra3018@gmail.com>
2024-03-11 20:09:51 -0400

    now not all destinations will be shown

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,12 +1,14 @@
 class DestinationsController < ApplicationController
   def index
-    
-    @destinations = Destination.all
-  end
-
-  def show
-    
-    @destination = Destination.find(params[:id])
+    if params[:destination_id].present?
+      # Find and display only the selected destination.
+      @destinations = [Destination.find(params[:destination_id])]
+    else
+      # If no destination is selected, you might want to show all or none.
+      @destinations = []
+      # Alternatively, to show all destinations when none is selected, use Destination.all
+    end
   end
 end
+
 
